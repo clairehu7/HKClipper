919a1bb5f34f1242c493361a22501ccabcec7e5c
clairehu <471339423@qq.com>
2016-08-17 21:18:19 +0800

    add

---
+++ HKClipperDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HKClipperDemo
+//
+//  Created by hukaiyin on 16/8/17.
+//  Copyright © 2016年 hukaiyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HKClipperDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HKClipperDemo
+//
+//  Created by hukaiyin on 16/8/17.
+//  Copyright © 2016年 hukaiyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
